85ed51882fff702e08507e551eab6cbe60c37e0e
kimgukju <kimgukju@kimgukju.com>
2023-04-12 16:31:58 +0900

    숫자데이터

---
+++ hello/src/com/callor/hello/HelloGwangju.java
@@ -0,0 +1,16 @@
+package com.callor.hello;
+
+public class HelloGwangju {
+	
+	public static void main(String[] args) {
+		/*
+		 * main() method는 프로젝트 전체에서 한번만 등장하는 것이 원칙이다
+		 * eclipse로 코드를 작성할때는 현재 보고있는 클래스 파일의 
+		 * main() method를 진입점 method로 본다 
+		 */
+		System.out.println("Hello Korea");
+		System.out.println(30 + 40);
+		System.out.println(30 * 40);
+		
+	}
+}
